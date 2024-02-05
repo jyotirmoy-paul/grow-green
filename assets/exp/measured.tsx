cb1e94fae41d8ae71480b4a5272df40c47b76120
Jyotirmoy Paul <mr.jyotirmoy.paul@gmail.com>
2024-02-05 11:53:46 +0530

    Add coordinate transformations support & tap detection for farms

--- assets/exp/measured.tsx
+++ assets/exp/measured.tsx
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="measured" tilewidth="1024" tileheight="1638" tilecount="3" columns="0" objectalignment="topleft">
- <grid orientation="isometric" width="1" height="1"/>
- <tile id="0">
-  <image width="1024" height="1638" source="../images/tiles/hill.png"/>
+<tileset version="1.10" tiledversion="1.10.2" name="world-tiles" tilewidth="1024" tileheight="1638" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image width="1024" height="1466" source="../images/tiles/grass-.png"/>
  </tile>
- <tile id="1">
-  <image width="1024" height="1638" source="../images/tiles/village.png"/>
+ <tile id="4">
+  <image width="1024" height="1638" source="../images/tiles/village-.png"/>
  </tile>
- <tile id="8">
-  <image width="1024" height="1466" source="../images/tiles/grass.png"/>
+ <tile id="5">
+  <image width="1024" height="1638" source="../images/tiles/hill-.png"/>
  </tile>
 </tileset>
--- lib/game/game/grow_green_world.dart
+++ lib/game/game/grow_green_world.dart
@@ -1,46 +1,77 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
-import 'package:flame/events.dart';
+import 'package:flame/extensions.dart';
+import 'package:flame/sprite.dart';
+import 'package:flame/src/game/notifying_vector2.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:growgreen/game/game/grow_green_game.dart';
+import 'package:growgreen/game/utils/game_utils.dart';
 
 class GrowGreenWorld extends World with HasGameRef<GrowGreenGame> {
-  late TiledComponent mapp;
+  late TiledComponent worldMap;
   late ObjectGroup farms;
-  Function(Vector2 center) getCenter;
+  late Selector selector;
 
-  GrowGreenWorld(
-    this.getCenter,
-  );
+  Function(Vector2 center)? onWorldLoad;
 
-  @override
-  FutureOr<void> onLoad() async {
-    const height = 640.0;
-    mapp = await TiledComponent.load(
-      'world.tmx',
-      Vector2(height * 1.6, height),
-      prefix: 'assets/exp/',
-    );
+  GrowGreenWorld();
 
-    mapp.anchor = Anchor.topLeft;
+  void _initialize() {
+    /// initialize game world size
+    GameUtils.initializeWithWorldSize(worldMap.size);
 
-    add(mapp);
-    getCenter(mapp.absoluteCenter);
+    /// callback to notify game center
+    onWorldLoad?.call(worldMap.absoluteCenter);
 
-    final farmsObjectGroup = mapp.tileMap.getLayer<ObjectGroup>('Farms');
-    // final grounds = mapp.tileMap.getLayer<TileLayer>('Ground');
+    /// initialize farms
+    final farmsObjectGroup = worldMap.tileMap.getLayer<ObjectGroup>('Farms');
 
     if (farmsObjectGroup == null) {
       throw Exception('"Farms" layer not found! Have you added it in the map?');
     } else {
       farms = farmsObjectGroup;
     }
+  }
 
-    // for (final farm in farms.objects) {
-    //   // farm.polygon.contains();
-    // }
+  @override
+  FutureOr<void> onLoad() async {
+    worldMap = await TiledComponent.load(
+      'world.tmx',
+      GameUtils.tileSize,
+      prefix: 'assets/exp/',
+    )
+      ..anchor = Anchor.topLeft;
 
-    return super.onLoad();
+    selector = Selector(
+      await gameRef.images.load('tiles/selector.png'),
+    );
+
+    await addAll([
+      worldMap,
+      selector,
+    ]);
+
+    _initialize();
+  }
+}
+
+class Selector extends SpriteComponent {
+  bool show = false;
+
+  Selector(Image image)
+      : super(
+          sprite: Sprite(image, srcSize: Vector2(1024, 1466)),
+          anchor: Anchor.bottomCenter,
+          position: Vector2(2048, 640 * 4),
+        );
+
+  @override
+  void render(Canvas canvas) {
+    if (!show) {
+      return;
+    }
+
+    super.render(canvas);
   }
 }
