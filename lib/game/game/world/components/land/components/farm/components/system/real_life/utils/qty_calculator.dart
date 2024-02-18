49b2886503ef4d4f8bf73be69282a8b8ac7a16f9
Jainam-IITK <jainamsb1@gmail.com>
2024-02-18 19:24:05 +0530

    Add soil damage calculator

--- lib/game/game/world/components/land/components/farm/components/system/real_life/utils/qty_calculator.dart
+++ lib/game/game/world/components/land/components/farm/components/system/real_life/utils/qty_calculator.dart
@@ -1,8 +1,10 @@
 // Values are per hacter
 
 import '../../../../../../../../../enums/system_type.dart';
+import '../../../../model/content.dart';
 import '../../../crop/enums/crop_type.dart';
 import '../../model/qty.dart';
+import '../calculators/crops/base_crop.dart';
 import '../systems/system.dart';
 
 abstract class QtyCalculator {
@@ -17,12 +19,39 @@ abstract class QtyCalculator {
     return System(systemType: systemType).seedQty(cropType);
   }
 
+  static Qty getFertilizerQtyRequiredFor({
+    required SystemType systemType,
+    required double soilHealthPercentage,
+    required CropType cropType,
+    double area = 10000,
+  }) {
+    final cropMaxAgeInMonths = BaseCropCalculator.fromCropType(cropType).maxAgeInMonths;
+    return getFertilizerQtyRequiredFromTime(
+      soilHealthPercentage: soilHealthPercentage,
+      ageInMonths: cropMaxAgeInMonths,
+      area: area,
+    );
+  }
+
   // TODO : area is required and should not be assumed to be full one hacter
-  static Qty getFertilizerQtyRequired({required double soilHealthPercentage, double area = 10000}) {
+  static Qty getFertilizerQtyRequiredFromTime({
+    required double soilHealthPercentage,
+    required int ageInMonths,
+    double area = 10000,
+  }) {
+    // area factor is the area of land for which fertilizer is required with default being 1 hacter
     const hacterToSqMeter = 10000;
     final areaRatio = area / hacterToSqMeter;
     final qtyPerHectare = -5 * soilHealthPercentage + 50;
-    final qtyRequired = areaRatio * qtyPerHectare;
+
+    // Get the time factor
+    // time factor is the time for which fertilizer is required with default being 4 months
+    const referenceAgeInMonths = 4;
+    final cropMaxAgeInMonths = ageInMonths;
+    final ageRatio = cropMaxAgeInMonths / referenceAgeInMonths;
+
+    final qtyRequired = ageRatio * areaRatio * qtyPerHectare;
+
     return Qty(value: qtyRequired.round(), scale: Scale.units);
   }
 }
