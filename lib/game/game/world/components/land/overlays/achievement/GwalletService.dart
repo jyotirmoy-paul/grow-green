0fcf82f5ae36561042f52b7300734e1f704316a9
Jainam-IITK <jainamsb1@gmail.com>
2024-03-13 01:58:41 +0530

    Add Firebase Storage service and update dependencies

--- lib/game/game/world/components/land/overlays/achievement/GwalletService.dart
+++ lib/game/game/world/components/land/overlays/achievement/GwalletService.dart
@@ -12,6 +12,7 @@ enum GwalletClaimStatus {
 
 class GwalletService {
   static const tag = 'GwalletService';
+  static const _baseUrl = 'http://52.172.30.83:3000';
   static Future<void> launchUrl(String urlString) async {
     Log.d("url to launch: $urlString");
     try {
@@ -37,12 +38,49 @@ class GwalletService {
     }
   }
 
+  static Future<GwalletClaimStatus> claimGwalletFromFarmView(String farmLink) async {
+    try {
+      final url = await getFarmViewToken(farmLink);
+      if (url != null) {
+        await launchUrl(url);
+        return GwalletClaimStatus.success;
+      } else {
+        return GwalletClaimStatus.failed;
+      }
+    } catch (e) {
+      Log.e('$tag Failed to claim Gwallet');
+      return GwalletClaimStatus.failed;
+    }
+  }
+
+  static Future<String?> getFarmViewToken(String farmLink) async {
+    final dio = Dio();
+
+    final url = '$_baseUrl/viewFarmToken/?farmlink=$farmLink';
+    const bodyUrl = '$_baseUrl/viewFarmToken/';
+    final body = {
+      'farmlink': farmLink,
+    };
+    final response = await dio.post(
+      bodyUrl,
+      data: body,
+    );
+
+    if (response.statusCode == 200) {
+      Log.i('$tag Gwallet claimed');
+      return response.data.toString();
+    } else {
+      Log.e('$tag Failed to claim Gwallet');
+      return null;
+    }
+  }
+
   static Future<String?> getWalletAddUrlFromChallenge(ChallengeModel challenge) async {
     final dio = Dio();
     final amount = challenge.offer.value.value;
     final code = challenge.offer.code;
 
-    final url = 'http://52.172.30.83:3000/token/?amount=$amount&code=$code';
+    final url = '$_baseUrl/token/?amount=$amount&code=$code';
     final response = await dio.get(url);
 
     if (response.statusCode == 200) {
